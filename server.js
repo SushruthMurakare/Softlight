8b6eb77456af15df20c67adc3d0d38e22ead438d
SushruthMurakare <sushruthraomurakare@gmail.com>
2025-11-04 14:37:27 -0700

    port

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'frontend.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
   console.log(`📂 Open your browser to http://localhost:${PORT}`);
 });
